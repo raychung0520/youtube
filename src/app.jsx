d9e571c0fdbbb99eac5f608268358833c469733a
forainst <raychung0520@gmail.com>
2021-07-22 14:43:58 -0700

    separated a network logic into service - refactoring

--- src/app.jsx
+++ src/app.jsx
@@ -1,24 +1,22 @@
 import React, { useEffect, useState } from "react";
-import "./app.css";
+import styles from "./app.module.css";
+import SearchHeader from "./components/search_header/search_header";
 import VideoList from "./components/video_list/video_list";
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]); // React Hook
+  const search = (query) => {
+    youtube.search(query).then((videos) => setVideos(videos));
+  };
   useEffect(() => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(
-      "https://youtube.googleapis.com/youtube/v3/videos?key=AIzaSyB5og8jiif1YIuh8HvKV7xqB8cp8nfZKi4&part=snippet&maxResults=25&chart=mostPopular&key=AIzaSyB5og8jiif1YIuh8HvKV7xqB8cp8nfZKi4",
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => setVideos(result.items))
-      .catch((error) => console.log("error", error));
+    youtube.mostPopular().then((videos) => setVideos(videos));
   }, []); // second parameter, when change of second parameter, the function would be called
-  return <VideoList videos={videos} />;
+  return (
+    <div className={styles.app}>
+      <SearchHeader onSearch={search} />
+      <VideoList videos={videos} />
+    </div>
+  );
 }
 
 export default App;
